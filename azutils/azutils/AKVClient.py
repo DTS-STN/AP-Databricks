473c333e01b53853571ff32bd9b9abe7cd174127
arpit-dhindsa <90341407+arpit-dhindsa@users.noreply.github.com>
2021-12-20 13:50:56 -0700

    Update AKVClient.py

--- azutils/azutils/AKVClient.py
+++ azutils/azutils/AKVClient.py
@@ -9,61 +9,65 @@ class AKVClient:
     without knowing the databricks secret scope name and azure key vault secrets.
     """
 
-    # store databricks secret scope name in a variable
-    secretScopeName = "akv-saeb-dbc-scrt-scp"
-
     # get or create spark session 
     spark = SparkSession.builder.getOrCreate()
     # access DBUtils module 
     dbutils = DBUtils(spark)
 
-
-    # empty __init__ method as the class doesn't require any attributes to be passed
+    # class takes no parameters
     def __init__(self):
-        pass
+       pass
 
-    # Below are the class methods that need to be called on the class itself to access the azure key vault secrets   
+    # Below are the class methods that can be called to return secrets from azure key vault secrets  
+    @classmethod
+    def dbcSecretScopeName(cls):
+       """
+       Provides access to Databricks Secret Scope Name.
+       """
+       secretScopeName = "akv-saeb-dbc-scrt-scp"
+       return cls.dbutils.secrets.get(scope = secretScopeName, key = "dbc-secret-scope")
+
     @classmethod
     def jdbcServerName(cls):
         """
         Provides access to Azure Synapse SQL Dedicated Pool Server Name.
         """
-        return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "sqlpool01-servername")
+        return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "sqlpool01-servername")
    
     @classmethod
     def jdbcDatabase(cls):
        """
        Provides access to Azure Synapse SQL Dedicated Pool Database Name.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "sqlpool01-dbname")
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "sqlpool01-dbname")
 
     @classmethod
     def jdbcUsername(cls):
        """
        Provides access to Azure Synapse SQL Dedicated Pool Username.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "sqlpool01-user")
-
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "sqlpool01-user")
+   
     @classmethod
     def jdbcPassword(cls):
        """
        Provides access to Azure Synapse SQL Dedicated Pool Password.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "sqlpool01-pwd")
-    
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "sqlpool01-pwd")
+   
     @classmethod
     def jdbcHost(cls):
        """
        Provides access to Azure Synapse SQL Dedicated Pool JDBC Host.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "sqlpool01-host")
-
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "sqlpool01-host")
+   
     @classmethod
     def jdbcPort(cls):
        """
        Provides access to Azure Synapse SQL Dedicated Pool JDBC Port.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "sqlpool01-port")
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "sqlpool01-port")
     
     @classmethod
     def jdbcUrl(cls):
@@ -72,76 +76,76 @@ class AKVClient:
        """
        extra_options = "encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.sql.azuresynapse.net;loginTimeout=30;"
        return f"jdbc:sqlserver://{cls.jdbcServerName()}:{cls.jdbcPort()};database={cls.jdbcDatabase()};{extra_options};"
-
+   
     @classmethod
     def statscanPassword(cls):
        """
        Provides Password to access to Government of Canada API Store.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "statscan-password")
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "statscan-password")
 
     @classmethod
     def statscanUsername(cls):
        """
        Provides Username to access to Government of Canada API Store.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "statscan-username")
-    
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "statscan-username")    
+   
     @classmethod
     def saebDbwAdfToken(cls):
        """
        Provides access to Databricks Token for Azure Data Factory Connection.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "saebdbwadftoken")
-
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "saebdbwadftoken")
+   
     @classmethod
     def adlsAccessKey(cls):
        """
        Provides access to Azure Data Lake Storage Access Key.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "saebadlsstorageaccesskey")
-
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "saebadlsstorageaccesskey")
+   
     @classmethod
     def adlsConnectionString(cls):
        """
        Provides access to Azure Data Lake Storage Connection String.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "saebadlsstorage-connection-string")
-
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "saebadlsstorage-connection-string")
+   
     @classmethod
     def adlsName(cls):
        """
        Provides access to Azure Data Lake Storage Name.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "saebadlsstoragename")
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "saebadlsstoragename")
 
     @classmethod
     def adobeAnalyticsClientID(cls):
        """
        Provides access to Adobe Analytics Client ID.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "aa-client-id")
-
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "aa-client-id")
+   
     @classmethod
-    def adobeAnalyticsCLientSecret(cls):
+    def adobeAnalyticsClientSecret(cls):
        """
        Provides access to Adobe Analytics Client Secret.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "aa-client-secret")
-
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "aa-client-secret")
+   
     @classmethod
     def adobeAnalyticsGlobalCompanyID(cls):
        """
        Provides access to Adobe Analytics Global Company ID.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "aa-global-company-id")
-
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "aa-global-company-id")
+   
     @classmethod
     def adobeAnalyticsOrgID(cls):
        """
        Provides access to Adobe Analytics Organization ID.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "aa-org-id")
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "aa-org-id")
 
     @classmethod
     def adobeAnalyticsPrivateKey(cls):
@@ -149,26 +153,19 @@ class AKVClient:
        Provides access to Adobe Analytics Private Key.
        """
        # returns private key in bytes format
-       private_key =  AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "aa-private-key")
+       private_key =  cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "aa-private-key")
        return ("-----BEGIN PRIVATE KEY-----\n"+private_key+"\n-----END PRIVATE KEY-----").encode()
-
+   
     @classmethod
     def adobeAnalyticsReportSuiteID(cls):
        """
        Provides access to Adobe Analytics Report Suite ID.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "aa-report-suite-id")
-
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "aa-report-suite-id")
+   
     @classmethod
     def adobeAnalyticsSubjectAccount(cls):
        """
        Provides access to Adobe Analytics Subject Account.
        """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "aa-subject-account")
-
-    @classmethod
-    def dbcSecretScopeName(cls):
-       """
-       Provides access to Databricks Secret Scope Name.
-       """
-       return AKVClient.dbutils.secrets.get(scope = AKVClient.secretScopeName, key = "dbc-secret-scope")
+       return cls.dbutils.secrets.get(scope = cls.dbcSecretScopeName(), key = "aa-subject-account")
